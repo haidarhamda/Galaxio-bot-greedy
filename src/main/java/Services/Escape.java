dc374f8c79fc339ad8225a3d880e7419ca698a0f
BreezyDR <88769406+BreezyDR@users.noreply.github.com>
2023-02-16 06:17:41 +0700

    Create Escape.java

---
+++ src/main/java/Services/Escape.java
@@ -0,0 +1,25 @@
+package Services;
+import Enums.*;
+import Models.*;
+
+
+import java.util.*;
+import java.util.stream.*;
+
+import static java.lang.Math.*;
+import static java.lang.Math.pow;
+
+public class Escape {
+    private static GameObject bot;
+    private static GameState gameState;
+    private static GameObject avoidedObjects;
+
+    public static PlayerActions playerAction;
+    public static int heading;
+    public int getHeading(){return heading;}
+
+    public static void escape_radius() {
+        
+    }
+    
+}
