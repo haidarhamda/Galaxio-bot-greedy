381abe00c593d6c1f8cdb96f7e7b76e939e33915
Rezapu Obed <obed.ubaidillah@gmail.com>
2023-02-16 23:59:04 +0700

    ntahlah apa ini

--- src/main/java/Services/Chase.java
+++ src/main/java/Services/Chase.java
@@ -3,8 +3,6 @@ package Services;
 import Models.*;
 import Enums.*;
 import java.util.*;
-import java.util.List;
-import java.util.ArrayList;
 import java.util.stream.*;
 
 
@@ -18,8 +16,10 @@ public class Chase {
     private static GameState gameState;
 
     public static PlayerActions playerAction;
-    public static GameObject prey;
+    public static GameObject aimPrey;
+    public static GameObject chasePrey;
     public static int heading;
+    public static boolean aiming = true;
 
     private static final double MAX_CHASE_TIME = 5.0;
     private static final Integer TORPEDO_SPEED = 60;
@@ -30,52 +30,81 @@ public class Chase {
     }
 
     public static void setAction(){
-        getPrey();
-        if(Chase.prey!=null){
-            Chase.heading = Algorithm.getHeadingBetween(Chase.bot, Chase.prey);
-            System.out.println("at "+Chase.heading);
-        }else{
-            Chase.playerAction = PlayerActions.STOP;
+        Chase.getPrey();
+        if(Chase.chasePrey != null){
+            Chase.heading = Algorithm.getHeadingBetween(Chase.bot, Chase.chasePrey);
+            Chase.playerAction = PlayerActions.FORWARD;
         }
+        if(Chase.aimPrey != null){
+            if(Chase.aiming){
+                Chase.heading = Algorithm.getHeadingBetween(Chase.bot, Chase.aimPrey);
+                Chase.playerAction = PlayerActions.FIRETORPEDOES;
+                Chase.aiming = false;
+            }else if(Chase.chasePrey == null){
+                Chase.heading = Algorithm.getHeadingBetween(Chase.bot, Chase.aimPrey);
+                Chase.playerAction = PlayerActions.FORWARD;
+                Chase.aiming = true;
+            }else{
+                Chase.aiming = true;
+            }
+
+        }
+        // if(Chase.aiming){
+
+        //     Chase.aiming = false;
+        // }
+        // System.out.println("shooter "+Chase.bot.id);
+        // System.out.println("prey "+Chase.prey.id);
+        // System.out.println("at "+Chase.heading);
     }
 
-    public static GameObject getPrey(){
-        var prey = findShootable();
-        if(prey != null){ // is any shootable?
-            Chase.prey = prey;
-            Chase.playerAction = PlayerActions.FIRETORPEDOES;
+    public static void getPrey(){
+        Chase.aimPrey = findShootable();
+        if(aimPrey != null){ // is any shootable?            
+            
             System.out.println("Shooting");
-        }else{
-            prey = findChaseable();
-            if(prey != null){ // is any chaseable?
-                Chase.prey = prey;
-                Chase.playerAction = PlayerActions.FORWARD;
-                System.out.println("Chasing");
-            }
         }
-        return prey;
+        Chase.chasePrey = findChaseable();
+        if(chasePrey != null){ // is any chaseable?
+            
+            System.out.println("Chasing");
+        }
     }
     
     public static GameObject findChaseable(){
         var objListByBorderDistance = Algorithm.getObjectsByBorderDistance(bot, ObjectTypes.PLAYER, Chase.gameState);
-        objListByBorderDistance.remove(0); // remove self
+        objListByBorderDistance.remove(Chase.bot); // remove self
+
+        var gasClouds = Algorithm.getObjectsByDistance(bot, ObjectTypes.GAS_CLOUD, gameState, false);
+        var wormholes = Algorithm.getObjectsByDistance(bot, ObjectTypes.WORMHOLE, gameState, false);
         
         for(GameObject bot : objListByBorderDistance){
-            if(Algorithm.predictCollisionTime(Chase.bot, bot) <= MAX_CHASE_TIME && bot.size*2 <= Chase.bot.size)
-            return bot;
+            if(Algorithm.predictCollisionTime(Chase.bot, bot) <= MAX_CHASE_TIME && bot.size*2 <= Chase.bot.size){
+                if(!Algorithm.checkCollision(Chase.bot, bot, gasClouds) && 
+                !Algorithm.checkCollision(Chase.bot, bot, wormholes))
+                return bot;
+            }
         }
         return null;
     }
 
     public static GameObject findShootable(){
         var objListBySize = Algorithm.getObjectsBySize(ObjectTypes.PLAYER, gameState);
-        objListBySize.remove(0); // remove self
+        objListBySize.remove(Chase.bot); // remove self
+
+        var gasClouds = Algorithm.getObjectsByDistance(bot, ObjectTypes.GAS_CLOUD, gameState, false);
+        var asteroids = Algorithm.getObjectsByDistance(bot, ObjectTypes.ASTEROID_FIELD, gameState, false);
+        var wormholes = Algorithm.getObjectsByDistance(bot, ObjectTypes.WORMHOLE, gameState, false);
 
         for(GameObject bot : objListBySize){
             var distance = Algorithm.getDistanceBetween(Chase.bot, bot);
             var torpedoTravelTime = Algorithm.toTime(TORPEDO_SPEED, distance);
-            if(Algorithm.calcualteCenterToBorderTime(bot) >= torpedoTravelTime)
-                return bot;
+            if(Algorithm.calcualteCenterToBorderTime(bot) >= torpedoTravelTime){
+                if(!Algorithm.checkCollision(Chase.bot, bot, gasClouds) && 
+                !Algorithm.checkCollision(Chase.bot, bot, asteroids) &&
+                !Algorithm.checkCollision(Chase.bot, bot, wormholes))
+                    return bot;
+            }
         }
         return null;
     }
