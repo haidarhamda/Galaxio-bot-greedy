37df763c3099c7714119e23c61b819870241b073
BreezyDR <88769406+BreezyDR@users.noreply.github.com>
2023-02-16 12:38:26 +0700

    Update Escape.java

--- src/main/java/Services/Escape.java
+++ src/main/java/Services/Escape.java
@@ -19,13 +19,37 @@ public class Escape {
         Escape.bot = bot;
         Escape.gameState = gameState;
 
-        // escape_now();
+        escape_now();
         Escape.heading = Algorithm.getHeadingBetween(Escape.bot, Escape.avoidedObjects);
 
     }
 
-    public static void escape_radius() {
-        
+    public static void escape_now() {
+        var radius = detectRadius();
+        if (radius != null) {
+            Escape.avoidedObjects = radius;
+            if (bot.getSize() > 50) {
+                Escape.playerAction = PlayerAction.START_AFTERBURNER;
+            } else if (getRadius() > 30) {
+                Escape.playerAction = PlayerAction.FIRE_TELEPORTER;
+                Escape.playerAction = PlayerAction.TELEPORT;
+            } else {
+                Escape.playerAction = PlayerAction.FORWARD;
+            }
+        } else {
+            var other_ship = detectship();
+            if (other_ship != null && other_ship.getSize() <= bot.getSize() - 10) {
+                Escape.avoidedObjects = other_ship;
+                if (bot.getSize() > 50) {
+                    Escape.playerAction = PlayerAction.START_AFTERBURNER;
+                } else if (getRadius() > 30) {
+                    Escape.playerAction = PlayerAction.FIRE_TELEPORTER;
+                    Escape.playerAction = PlayerAction.TELEPORT;
+                } else {
+                    Escape.playerAction = PlayerAction.FORWARD;
+                }
+            }
+        }
     }
     
 }
